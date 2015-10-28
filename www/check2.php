f1d2b77686f7e758834125843f9aee7d6ab04dad
Leo Lin <leolin@exosite.com>
2015-10-28 17:01:44 +0800

    remove some files

--- www/check2.php
+++
@@ -1,9 +0,0 @@
-<?
-$con = mysql_connect('localhost','root','0000');
-mysql_select_db("newdatabase",$con);
-$name=$_POST['name'];
-$age=$_POST['age'];
-$email=$_POST['email'];
-mysql_query("insert into newtable(name,age,email)values('{$name}','{$age}','{$email}')");
-//mysql_close();
-?>
--- www/time.php
+++
@@ -1,4 +0,0 @@
- <?
- $t=strtotime(date ("Y-m-d H:i:s" ,mktime(date('H')+8, date('i'), date('s'), date('m'), date('d'), date('Y'))));
- echo $t;
- ?>
